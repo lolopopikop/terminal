84ede86e1256322f7fdbb16f0d8cd30d7c722c9d
lolopopikop <lolopopikop@gmail.com>
2025-10-03 13:45:07 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
 int main(){
+  while (true){
+    std::cout << "$ ";
+    std::string command;
 
+    std::getline(std::cin, command);
+    std::cout << command << std::endl;
+  }
 
   return 0;
 }
