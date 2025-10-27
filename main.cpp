3684b4e539dc87d99011eda85222bc3da045f8fd
lolopopikop <lolopopikop@gmail.com>
2025-10-27 19:50:02 +0300

    4

--- main.cpp
+++ main.cpp
@@ -9,14 +9,30 @@ int main(){
     std::cout << "$ ";
     std::getline(std::cin, command);
 
-    if (command[0] == '\\' && command[1] == 'q') return 0;
-
-    while (true){
-      std::cout << command << std::endl
-                << "Press ENTER to display again" << std::endl;
-      std::string temp;
-      std::getline(std::cin, temp);
+    if (command.substr(0, 13) == "print history"){
+      std::ifstream file("kubsh_history");
+      if (!file)
+        std::cout << "No history yet!" << std::endl;
+      else{
+        std::string komanda;
+        while (std::getline(file, komanda))
+          std::cout << komanda << std::endl;
+      }
     }
+
+    else if (command.substr(0, 13) == "clear history"){
+      if (std::remove("kubsh_history") == 0)
+        std::cout << "History cleared!" << std::endl;
+      else
+        std::cout << "No history file found!" << std::endl;
+    }
+
+    if (command.substr(0, 13) != "print history" && command.substr(0, 13) != "clear history"){
+      std::ofstream file("kubsh_history", std::ios::app);
+      file << command << std::endl;
+    }
+
+    if (command.substr(0, 2) == "\\q") return 0;
   }
 
   return 0;
