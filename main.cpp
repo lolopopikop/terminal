27beca67df51cfc7cfc847a2bd62474078a9d1d8
lolopopikop <lolopopikop@gmail.com>
2025-10-27 19:42:14 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 
 int main(){
   std::string command;
   while (true){
     std::cout << "$ ";
     std::getline(std::cin, command);
+
+    if (command[0] == '\\' && command[1] == 'q') return 0;
 
     while (true){
       std::cout << command << std::endl
